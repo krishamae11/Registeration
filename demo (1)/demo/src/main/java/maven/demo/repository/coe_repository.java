701e403829f457509d9e889314e0289b75e78125
krishamae11 <krishamaerespeto@gmail.com>
2026-05-05 14:44:27 +0800

    Update and rename coe_repository.java to coe_admin_repository.java

--- demo (1)/demo/src/main/java/maven/demo/repository/coe_repository.java
+++
@@ -1,7 +0,0 @@
-package maven.demo.repository;
-
-import maven.demo.model.coe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface coe_repository extends JpaRepository<coe, Long> {
-}
