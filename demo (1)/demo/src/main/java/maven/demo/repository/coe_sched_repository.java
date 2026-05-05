2b191a238e0e063eb7495b6a07937a4cbccf61e5
krishamae11 <krishamaerespeto@gmail.com>
2026-05-05 14:54:59 +0800

    Update coe_sched_repository.java

--- demo (1)/demo/src/main/java/maven/demo/repository/coe_sched_repository.java
+++ demo (1)/demo/src/main/java/maven/demo/repository/coe_sched_repository.java
@@ -1,7 +1,7 @@
 package maven.demo.repository;
 
-import maven.demo.model.coeupper;
+import maven.demo.model.coe_admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface coe_repository_upper extends JpaRepository<coe_upper, Long> {
+public interface coe_sched_repository extends JpaRepository<coe_admin, Long> {
 }
