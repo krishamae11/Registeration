d4dad9a513dad77fdd3554b0da0f05f0fae032bd
krishamae11 <krishamaerespeto@gmail.com>
2026-05-05 14:43:10 +0800

    Update and rename coe_controller_upper.java to coe_admin_controller.java

--- demo (1)/demo/src/main/java/maven/demo/controller/coe_admin_controller.java
+++ demo (1)/demo/src/main/java/maven/demo/controller/coe_admin_controller.java
@@ -1,21 +1,21 @@
 package maven.demo.controller;
 
-import maven.demo.repository.coe_repository;
-import maven.demo.model.coe;
+import maven.demo.repository.coe__admin_repository;
+import maven.demo.model.coe_admin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/api/coeupper")
+@RequestMapping("/api/studentinfo")
 @CrossOrigin(origins = "*")
-public class coe_controller_upper {
+public class coe_admin_controller {
 
     @Autowired
-    private coe_repository repo;
+    private coe_admin_repository repo;
 
     @GetMapping // your custom endpoint
-    public List<coe> getAllSchedules() {
+    public List<coe_admin> getAllSchedules() {
         return repo.findAll();
     }
 }
