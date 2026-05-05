7ed1fe5d6878c4d7f904b920914826285a203c0c
krishamae11 <krishamaerespeto@gmail.com>
2026-05-05 12:20:16 +0800

    Update admission_controller.java

--- demo (1)/demo/src/main/java/maven/demo/controller/admission_controller.java
+++ demo (1)/demo/src/main/java/maven/demo/controller/admission_controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/admission")
 @CrossOrigin(origins = "*")
-public class AdmissionController {
+public class admission_controller {
 
     @Autowired
     private admissionrepository repo;
