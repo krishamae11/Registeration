f241c74d8b4f90f2b3b9842097eb51087965d956
krishamae11 <krishamaerespeto@gmail.com>
2026-05-05 14:55:56 +0800

    Update coe_sched_ontroller.java

--- demo (1)/demo/src/main/java/maven/demo/controller/coe_sched_ontroller.java
+++ demo (1)/demo/src/main/java/maven/demo/controller/coe_sched_ontroller.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/api/coe")
+@RequestMapping("/api/studentsched")
 @CrossOrigin(origins = "*")
 public class coe_sched_controller {
 
