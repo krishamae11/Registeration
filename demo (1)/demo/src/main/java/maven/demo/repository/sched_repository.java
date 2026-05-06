5b9b51cb2093b8dee4cb300441192f8c9711e81c
krishamae11 <krishamaerespeto@gmail.com>
2026-05-06 08:58:28 +0800

    Update sched_repository.java

--- demo (1)/demo/src/main/java/maven/demo/repository/sched_repository.java
+++ demo (1)/demo/src/main/java/maven/demo/repository/sched_repository.java
@@ -1,6 +1,6 @@
 package maven.demo.repository;
 
-import maven.demo.model.sched_assess;
+import maven.demo.entity.sched_assess;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface sched_repository extends JpaRepository<sched_assess, Long> {
