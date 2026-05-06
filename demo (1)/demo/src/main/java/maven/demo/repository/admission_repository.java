cd78dd59d28911c7a8fd8eb3c4b11f85cef770f6
krishamae11 <krishamaerespeto@gmail.com>
2026-05-07 07:47:56 +0800

    Update admission_repository.java

--- demo (1)/demo/src/main/java/maven/demo/repository/admission_repository.java
+++ demo (1)/demo/src/main/java/maven/demo/repository/admission_repository.java
@@ -4,4 +4,5 @@ import maven.demo.entity.admin_assess;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface admission_repository extends JpaRepository<admin_assess, Long> {
+    boolean existsByIdAndValidatedTrue(Long id);
 }
