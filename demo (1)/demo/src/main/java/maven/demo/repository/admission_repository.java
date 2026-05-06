844ca31e3931566d5a13f35acafd6eba7720e39e
krishamae11 <krishamaerespeto@gmail.com>
2026-05-06 08:57:00 +0800

    Update admission_repository.java

--- demo (1)/demo/src/main/java/maven/demo/repository/admission_repository.java
+++ demo (1)/demo/src/main/java/maven/demo/repository/admission_repository.java
@@ -1,6 +1,6 @@
 package maven.demo.repository;
 
-import maven.demo.model.admin_assess;
+import maven.demo.entity.admin_assess;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface admission_repository extends JpaRepository<admin_assess, Long> {
