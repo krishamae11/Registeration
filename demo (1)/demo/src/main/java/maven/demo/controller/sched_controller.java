c7e4e7c86fb4d7d0207842bbd5e5f54e4cc24e35
krishamae11 <krishamaerespeto@gmail.com>
2026-05-16 21:06:25 +0800

    Update sched_controller.java

--- demo (1)/demo/src/main/java/maven/demo/controller/sched_controller.java
+++ demo (1)/demo/src/main/java/maven/demo/controller/sched_controller.java
@@ -1,41 +1,37 @@
-/*@RestController
-@RequestMapping("/api")
-@CrossOrigin(origins = "*")
-public class TestController {
-
-    @GetMapping("/schedules")
-    public List<Map<String, String>> getSchedules() {
-
-        List<Map<String, String>> list = new ArrayList<>();
-
-        // SAMPLE (later this comes from DB)
-        Map<String, String> s1 = new HashMap<>();
-        s1.put("subject", "CC5");
-        s1.put("schedule", "3-5 pm T ITRM3");
-
-        list.add(s1);
-
-        return list;
-    }
-};*/
 package maven.demo.controller;
 
 import maven.demo.entity.sched_assess;
 import maven.demo.repository.sched_repository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/schedule")
 @CrossOrigin(origins = "*")
 public class sched_controller {
 
-    @Autowired
-    private sched_repository repo;
+    private final sched_repository repo;
 
-    @GetMapping // your custom endpoint
-    public List<sched_assess> getAllSchedules() {
-        return repo.findAll();
+    public sched_controller(sched_repository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping("/filter")
+    public List<sched_assess> filterSchedule(
+            @RequestParam String section,
+            @RequestParam String academicYear,
+            @RequestParam String semester
+    ) {
+
+        System.out.println("SECTION = [" + section + "]");
+        System.out.println("YEAR = [" + academicYear + "]");
+        System.out.println("SEM = [" + semester + "]");
+
+        return repo.filterSchedule(
+                section.trim(),
+                academicYear.trim(),
+                semester.trim()
+        );
     }
 }
